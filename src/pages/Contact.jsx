1bea3e776ab0fa81b8d98e40cdc418872f2dc6fe
Shivin Antony <shivinantony087@gmail.com>
2024-05-14 13:46:08 +0530

    ContactUs edited

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,14 @@
-import NavBar from "../components/NavBar"
+import NavBar from "../components/NavBar";
+import ContactUs from "../components/ContactUs";
 const Contact = () => {
   return (
     <>
-     <div className="bg-black h-screen w-screen ">
-         <NavBar />
-     </div>
+      <div className="bg-black h-screen w-screen  text-white relative  ">
+        <NavBar />
+        <ContactUs />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
